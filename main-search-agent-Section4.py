04ad30aca8cad6463d91df291f9a7b3e3c9d0086
UMB200 <UMB200@users.noreply.github.com>
2026-01-26 12:17:39 -0500

    Updated Search Agent with Structured Output

--- main-search-agent-Section4.py
+++ main-search-agent-Section4.py
@@ -7,7 +7,6 @@ from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 
@@ -18,30 +17,28 @@ load_dotenv()
 
 tavily_tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
-output_parsers = PydanticOutputParser(pydantic_object=AgentResponse)
+
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"],
-).partial(format_instructions=output_parsers.get_format_instructions())
+).partial(format_instructions="")
 
 agent = create_react_agent(
     llm=llm, tools=tavily_tools, prompt=react_prompt_with_format_instructions
 )
 agent_executor = AgentExecutor(agent=agent, tools=tavily_tools, verbose=True)
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parsers.parse(x))
-chain = agent_executor | extract_output | parse_output
-
+chain = agent_executor | extract_output | structured_llm
 
 def main():
     result = chain.invoke(
         input={
-            "input": "search for 3 job postings for a Sr. IT PM using AI in DC area on LinkedIn and list their details"
+            "input": "search for 3 job postings for a IT Project Manager using AI in DC area on LinkedIn and list their details"
         }
     )
     print(result)
-
 
 if __name__ == "__main__":
     main()
